f8c7b76982dfcfaf2d71ca035721fe19013e5494
kubarenatioN <[alwayswannaflyinbluesky@gmail.com]>
2021-09-20 18:51:40 +0300

    fix: location api

--- src/app/core/services/location.service.ts
+++ src/app/core/services/location.service.ts
@@ -1,18 +1,35 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { BehaviorSubject } from 'rxjs'
+import { concatMap } from 'rxjs/operators'
+
+export interface IUserCoords {
+  latitude: number
+  longitude: number
+}
 
 export interface IUserLocation {
-  city: string
+  results: [
+    {
+      components: {
+        city: string
+      }
+    }
+  ]
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
-  private ipApiUrl = 'http://ip-api.com/json/?lang=ru'
+  // private ipApiUrl = 'http://ip-api.com/json/?lang=ru'
+  private coordsApi = 'https://ipapi.co/json'
 
-  private location$$ = new BehaviorSubject<IUserLocation | null>(null)
+  private locationApi = 'https://api.opencagedata.com/geocode/v1/json'
+
+  private key = '03039f45941d4f4faabb20c82a9a5422'
+
+  private location$$ = new BehaviorSubject<string>('')
 
   location$ = this.location$$.asObservable()
 
@@ -21,12 +38,25 @@ export class LocationService {
   constructor(private http: HttpClient) {}
 
   getLocation(): void {
-    this.http.get<IUserLocation>(this.ipApiUrl).subscribe(location => {
-      this.location$$.next(location)
-    })
+    this.http
+      .get<IUserCoords>(this.coordsApi)
+      .pipe(
+        concatMap(coords =>
+          this.http.get<IUserLocation>(`${this.locationApi}`, {
+            params: {
+              q: `${coords.latitude}+${coords.longitude}`,
+              key: this.key,
+              language: 'native'
+            }
+          })
+        )
+      )
+      .subscribe(location => {
+        this.location$$.next(location.results[0].components.city)
+      })
   }
 
   changeLocation(id: number): void {
-    this.location$$.next({ city: this.locationsList[id] })
+    this.location$$.next(this.locationsList[id])
   }
 }
